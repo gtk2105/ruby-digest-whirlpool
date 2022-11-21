136dbb39120f0ca18f314be7f98bbd217a3afb57
gtk2105 <106717871+gtk2105@users.noreply.github.com>
2022-11-22 12:17:35 +1300

    Update extconf.rb

--- ext/digest/whirlpool/extconf.rb
+++ ext/digest/whirlpool/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 
 $defs << "-DHAVE_CONFIG_H"
-$INCFLAGS << " -I$(srcdir)/.. -I$(hdrdir)/ruby -I/usr/include/ruby-3.0.0/ruby"
+$INCFLAGS << " -I$(srcdir)/.. -I$(hdrdir)/ruby -I/usr/include/ruby-3.0.0/ruby/digest.h"
 
 have_header('ruby/digest.h')
 
